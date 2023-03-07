09f7af3e8156fdbf6cfd19748515c379d6c5ff40
Yasnovsky <andrey5.1997@mail.ru>
2023-03-07 18:11:41 +0500

    feat: #861mf9tac: update styles, icons for same look  with UI service

--- src/features/analytics/routes.tsx
+++ src/features/analytics/routes.tsx
@@ -1,5 +1,5 @@
-import { ReactComponent as IconAnalytics } from '../../assets/icons/analytics.svg';
-import { ReactComponent as IconAnalyticsActive } from '../../assets/icons/analytics-active.svg';
+import { ReactComponent as IconAnalytics } from '../../assets/icons/icon-analytics.svg';
+import { ReactComponent as IconAnalyticsActive } from '../../assets/icons/icon-analytics-active.svg';
 import { LINK_TO_DASHBOARD } from '../../common/config/config';
 
 export const analyticsSidebarRoutes = [
