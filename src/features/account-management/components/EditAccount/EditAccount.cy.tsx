3863b7f541bd4f4e4dc21c5f4730c4a62c087b74
Yasnovsky <andrey5.1997@mail.ru>
2023-04-24 15:50:56 +0500

    refactor(edit account): #85zrxxznr: moved the repetitive test code to a function that runs before each test

--- src/features/account-management/components/EditAccount/EditAccount.cy.tsx
+++ src/features/account-management/components/EditAccount/EditAccount.cy.tsx
@@ -2,37 +2,7 @@ import { LINK_TO_ACCOUNT_SERVICE, API_ROOT } from '../../../../common/config/con
 import EditAccount from './EditAccount';
 
 describe('EditAccount', () => {
-  it('SHOULD render no edit page WHEN there is component', () => {
-    mountComponent();
-
-    cy.getByData('edit-account')
-      .should('exist');
-  });
-
-  it('SHOULD call the backend to get data WHEN it wants to render corporate email', () => {
-    cy.intercept(
-      'GET',
-      `${API_ROOT}${LINK_TO_ACCOUNT_SERVICE}accounts/1`,
-      {
-        corporateEmail: 'test@tourmalinecore.com',
-        firstName: '',
-        lastName: '',
-        roles: [
-          {
-            id: 1,
-            name: 'CEO',
-          },
-        ],
-      },
-    );
-
-    mountComponent();
-
-    cy.getByData('corporate-email')
-      .should('have.text', 'test@tourmalinecore.com');
-  });
-
-  it('SHOULD call the backend to get data WHEN it wants to render first name input with value', () => {
+  beforeEach(() => {
     cy.intercept(
       'GET',
       `${API_ROOT}${LINK_TO_ACCOUNT_SERVICE}accounts/1`,
@@ -50,7 +20,19 @@ describe('EditAccount', () => {
     );
 
     mountComponent();
+  });
 
+  it('SHOULD render no edit page WHEN there is component', () => {
+    cy.getByData('edit-account')
+      .should('exist');
+  });
+
+  it('SHOULD call the backend to get data WHEN it wants to render corporate email', () => {
+    cy.getByData('corporate-email')
+      .should('have.text', 'test@tourmalinecore.com');
+  });
+
+  it('SHOULD call the backend to get data WHEN it wants to render first name input with value', () => {
     cy.getByData('first-name')
       .should('have.value', 'TestName');
   });
