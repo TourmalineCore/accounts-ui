db0fce89bbb9900788597e4d100d00be83e1d6f7
Yasnovsky <andrey5.1997@mail.ru>
2023-04-24 15:33:40 +0500

    feat(edit account): #85zrxxznr: add types, request for page and render corporate email

--- src/features/account-management/components/EditAccount/EditAccount.tsx
+++ src/features/account-management/components/EditAccount/EditAccount.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ContentCard from '../../../../components/ContentCard/ContentCard';
+import { LINK_TO_ACCOUNT_SERVICE } from '../../../../common/config/config';
+import { api } from '../../../../common/api';
+
+type EditAccountType = {
+  corporateEmail: string;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  roles: {
+    id: number;
+    name: string;
+  }[]
+};
 
 function EditAccount() {
+  const { id } = useParams();
+
+  // @ts-ignore
+  const [account, setAccount] = useState<EditAccountType>({});
+
+  useEffect(() => {
+    getEditAccountLoad();
+  }, []);
+
   return (
     <ContentCard>
       <section data-cy="edit-account" />
+
+      <span data-cy="corporate-email">{account.corporateEmail}</span>
+
     </ContentCard>
   );
+
+  async function getEditAccountLoad() {
+    const { data } = await api.get<EditAccountType>(`${LINK_TO_ACCOUNT_SERVICE}accounts/${id || 1}`);
+
+    setAccount(data);
+  }
 }
 
 export default EditAccount;
