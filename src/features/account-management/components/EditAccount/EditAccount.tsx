77ecbbd2e545dd9ff8235fb58db4088dde010714
Yasnovsky <andrey5.1997@mail.ru>
2023-04-24 14:05:58 +0500

    feat(edit account): #85zrxxznr: add empty component

---
+++ src/features/account-management/components/EditAccount/EditAccount.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentCard from '../../../../components/ContentCard/ContentCard';
+
+function EditAccount() {
+  return (
+    <ContentCard>
+      <section data-cy="edit-account" />
+    </ContentCard>
+  );
+}
+
+export default EditAccount;
