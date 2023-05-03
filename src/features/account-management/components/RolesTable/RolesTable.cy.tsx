01dc3a898da71e2fe6d83493d7c57fa07c262834
Ekaterina <echekina@gmail.com>
2023-05-03 16:31:42 +0500

    refactor(roles-page): #85zrx4muu: add network call

--- src/features/account-management/components/RolesTable/RolesTable.cy.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.cy.tsx
@@ -1,13 +1,14 @@
 import RolesTable from './RolesTable';
 
+const mockData = [{
+  id: 1,
+  name: 'Admin',
+  permissions: ['ViewPersonalProfile', 'EditPersonalProfile', 'ViewContacts'],
+}];
+
 describe('RolesTable', () => {
   it('SHOULD always show at least one role WHEN visit roles page', () => {
-    cy.intercept('GET', '/api/roles', [{
-      id: 1,
-      name: 'Admin',
-      permissions: ['ViewPersonalProfile', 'EditPersonalProfile', 'ViewContacts'],
-    },
-    ]);
+    cy.intercept('GET', '*/api/roles', mockData);
 
     cy.mount(
       <RolesTable />,
--- src/features/account-management/components/RolesTable/RolesTable.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { api } from '../../../../common/api';
+
+type RoleType = {
+  id: number,
+  name: string,
+  permissions: string[]
+};
 
 function RolesTable() {
-  const [roles, setRoles] = useState([{
-    id: 1,
-    name: 'Admin',
-    permissions: ['ViewPersonalProfile', 'EditPersonalProfile', 'ViewContacts'],
-  }]);
+  const [roles, setRoles] = useState<RoleType[]>([]);
+
+  useEffect(() => {
+    loadDataAsync();
+  }, []);
 
   return (
     <table data-cy="roles-table">
@@ -17,6 +24,11 @@ function RolesTable() {
       </tr>
     </table>
   );
+
+  async function loadDataAsync() {
+    const { data } = await api.get('api/roles');
+    setRoles(data);
+  }
 }
 
 export default RolesTable;
