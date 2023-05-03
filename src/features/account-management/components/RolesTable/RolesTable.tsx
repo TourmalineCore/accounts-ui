83ed1f88508d2407c75419eb6b058a935899bfde
Ekaterina <echekina@gmail.com>
2023-05-03 16:20:58 +0500

    feat(roles-page): #85zrx4muu: add at least one role

--- src/features/account-management/components/RolesTable/RolesTable.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RolesTable() {
+  const [roles, setRoles] = useState([{
+    id: 1,
+    name: 'Admin',
+    permissions: ['ViewPersonalProfile', 'EditPersonalProfile', 'ViewContacts'],
+  }]);
+
   return (
-    <div data-cy="roles-table">RolesTable</div>
+    <table data-cy="roles-table">
+      <tr data-cy="role-name-row">
+        <th>Permissions</th>
+        {roles.map((role) => (
+          <th>{role.name}</th>
+        ))}
+      </tr>
+    </table>
   );
 }
 
