072b7850e980bf3d98d051de2f99d11fb24c1003
Ekaterina <echekina@gmail.com>
2023-05-04 16:09:56 +0500

    feat(roles-page): #85zrx4muu: add profile section; use divs instead of table

--- src/features/account-management/components/RolesTable/RolesTable.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { api } from '../../../../common/api';
+import { Permissions } from '../../roles-enums';
 
 type RoleType = {
   id: number,
@@ -15,14 +16,32 @@ function RolesTable() {
   }, []);
 
   return (
-    <table data-cy="roles-table">
-      <tr data-cy="role-name-row">
-        <th>Permissions</th>
+    <div data-cy="roles-table" className="roles-table">
+      <div data-cy="role-name-row" className="roles-table__row" style={{ display: 'flex' }}>
+        <div className="roles-table__cell">Permissions</div>
         {roles.map((role) => (
-          <th>{role.name}</th>
+          <div className="roles-table__cell">{role.name}</div>
         ))}
-      </tr>
-    </table>
+      </div>
+      <div className="accordion roles-table__section">
+        <div className="accordion__header">
+          <div className="roles-table__row">My Profile</div>
+        </div>
+        <div data-cy="profile-section-content" className="accordion__content">
+          <div className="roles-table__row" style={{ display: 'flex' }}>
+            <div className="roles-table__cell">
+              {Permissions.ViewPersonalProfile}
+            </div>
+            <div className="roles-table__cell">
+              check
+            </div>
+            <div className="roles-table__cell">
+              check
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 
   async function loadDataAsync() {
