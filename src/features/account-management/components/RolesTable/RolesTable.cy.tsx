a5fcf01b5a77423f74b21016d421f1d6e8f6562b
Ekaterina <echekina@gmail.com>
2023-05-10 14:16:54 +0500

    test(roles-page): #85zrx4muu: add test for case when there are only roles

--- src/features/account-management/components/RolesTable/RolesTable.cy.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.cy.tsx
@@ -1,5 +1,27 @@
 import RolesTable from './RolesTable';
 
 describe('RolesTable', () => {
-  it('SHOULD always show at least one role WHEN visit roles page', () => {});
+  it('SHOULD show role columns and no permission rows WHEN there are roles and no permissions', () => {
+    mountComponent({
+      roles: ['Admin', 'Employee'],
+      permissions: [],
+    });
+
+    cy.getByData('role-column').first().contains('Admin');
+  });
 });
+
+function mountComponent({
+  roles,
+  permissions,
+}: {
+  roles: string[];
+  permissions: string[];
+}) {
+  cy.mount(
+    <RolesTable
+      roles={roles}
+      permissions={permissions}
+    />,
+  );
+}
