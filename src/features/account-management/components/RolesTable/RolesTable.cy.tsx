8e34866e89c8abca89429af11090264f8b0a1457
Ekaterina <echekina@gmail.com>
2023-05-10 14:31:03 +0500

    test(roles-page): #85zrx4muu: add test for case when there are only permission groups

--- src/features/account-management/components/RolesTable/RolesTable.cy.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.cy.tsx
@@ -3,23 +3,37 @@ import RolesTable from './RolesTable';
 describe('RolesTable', () => {
   it('SHOULD show role columns and no permission rows WHEN there are roles and no permissions', () => {
     mountComponent({
+      permissionGroups: [],
       roles: ['Admin', 'Employee'],
       permissions: [],
     });
 
     cy.getByData('role-column').first().contains('Admin');
   });
+
+  it('SHOULD show permission group rows WHEN there are permission groups and no roles or permissions', () => {
+    mountComponent({
+      permissionGroups: ['My Profile', 'Employees', 'Analytics', 'Account Management'],
+      roles: [],
+      permissions: [],
+    });
+
+    cy.getByData('permission-group').first().contains('My Profile');
+  });
 });
 
 function mountComponent({
+  permissionGroups,
   roles,
   permissions,
 }: {
+  permissionGroups: string[];
   roles: string[];
   permissions: string[];
 }) {
   cy.mount(
     <RolesTable
+      permissionGroups={permissionGroups}
       roles={roles}
       permissions={permissions}
     />,
