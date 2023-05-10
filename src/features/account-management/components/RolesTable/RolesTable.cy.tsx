1e0a050d05ca4e8a0d4909bad44a3caa8abf981d
Ekaterina <echekina@gmail.com>
2023-05-10 13:18:30 +0500

    chore(roles-page): #85zrx4muu: remove tests and markup for roles table

--- src/features/account-management/components/RolesTable/RolesTable.cy.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.cy.tsx
@@ -1,50 +1,5 @@
 import RolesTable from './RolesTable';
 
-const mockData = [{
-  id: 1,
-  name: 'Admin',
-  permissions: ['ViewPersonalProfile', 'EditPersonalProfile'],
-},
-{
-  id: 2,
-  name: 'Employee',
-  permissions: ['ViewPersonalProfile'],
-}];
-
 describe('RolesTable', () => {
-  it('SHOULD always show at least one role WHEN visit roles page', () => {
-    cy.intercept('GET', '*/api/roles', mockData);
-
-    cy.mount(
-      <RolesTable />,
-    );
-
-    cy.getByData('role-name-row')
-      .children()
-      .contains('Admin');
-  });
-
-  it('SHOULD show non-empty profile section WHEN visit roles page', () => {
-    cy.intercept('GET', '*/api/roles', mockData);
-
-    cy.mount(
-      <RolesTable />,
-    );
-
-    cy.getByData('profile-section-content')
-      .children()
-      .its('length')
-      .should('be.at.least', 1);
-  });
-
-  it('SHOULD show checked state for a permission WHEN this permission is in the list for this role', () => {
-    cy.intercept('GET', '*/api/roles', mockData);
-
-    cy.mount(
-      <RolesTable />,
-    );
-
-    cy.getByData('ViewPersonalProfile')
-      .contains('yes');
-  });
+  it('SHOULD always show at least one role WHEN visit roles page', () => {});
 });
--- src/features/account-management/components/RolesTable/RolesTable.tsx
+++ src/features/account-management/components/RolesTable/RolesTable.tsx
@@ -1,12 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { api } from '../../../../common/api';
-import { Permissions } from '../../roles-enums';
-
-type RoleType = {
-  id: number,
-  name: string,
-  permissions: string[]
-};
+import React from 'react';
 
 // ToDo
 // When create a new role, its object should be added to the beginning of the array using unshift method
@@ -18,78 +10,9 @@ type RoleType = {
 // based on this field, we will show either inputs or spans
 
 function RolesTable() {
-  const [roles, setRoles] = useState<RoleType[]>([]);
-
-  useEffect(() => {
-    loadDataAsync();
-  }, []);
-
   return (
-    <div data-cy="roles-table" className="roles-table">
-      <div data-cy="role-name-row" className="roles-table__row" style={{ display: 'flex' }}>
-        <div className="roles-table__cell">Permissions</div>
-        {roles.map((role) => (
-          <div className="roles-table__cell">{role.name}</div>
-        ))}
-      </div>
-      <div className="accordion roles-table__section">
-        <div className="accordion__header">
-          <div className="roles-table__row">My Profile</div>
-        </div>
-        <div data-cy="profile-section-content" className="accordion__content">
-          <div className="roles-table__row" style={{ display: 'flex' }}>
-            <div className="roles-table__cell">
-              {Permissions.ViewPersonalProfile}
-            </div>
-            {roles.map(({ name, permissions }) => (
-              <div className="roles-table__cell" data-cy="ViewPersonalProfile">
-                {permissions.map((permission) => (
-                  <div>{permission === 'ViewPersonalProfile' ? `${name}_${permission}_yes` : `${name}_${permission}_no`}</div>
-                ))}
-              </div>
-            ))}
-            {/* <div className="roles-table__cell" data-cy="ViewPersonalProfile">
-              {roles.map(({ permissions }) => (permissions.map((permission) => (
-                <div>{permission === 'ViewPersonalProfile' ? 'check' : 'uncheck'}</div>
-              ))))}
-            </div> */}
-          </div>
-
-          <div className="roles-table__row" style={{ display: 'flex' }}>
-            <div className="roles-table__cell">
-              {Permissions.EditPersonalProfile}
-            </div>
-            {roles.map(({ name, permissions }) => (
-              <div className="roles-table__cell" data-cy="EditPersonalProfile">
-                {permissions.map((permission) => (
-                  <div>{permission === 'EditPersonalProfile' ? `${name}_${permission}_yes` : `${name}_${permission}_no`}</div>
-                ))}
-              </div>
-            ))}
-            {/* <div className="roles-table__cell" data-cy="ViewPersonalProfile">
-              {roles.map(({ permissions }) => (permissions.map((permission) => (
-                <div>{permission === 'ViewPersonalProfile' ? 'check' : 'uncheck'}</div>
-              ))))}
-            </div> */}
-          </div>
-
-          {/* <div className="roles-table__row" style={{ display: 'flex' }}>
-            <div className="roles-table__cell">
-              {Permissions.EditPersonalProfile}
-            </div>
-            <div className="roles-table__cell">
-              uncheck
-            </div>
-          </div> */}
-        </div>
-      </div>
-    </div>
+    <div data-cy="roles-table" className="roles-table" />
   );
-
-  async function loadDataAsync() {
-    const { data } = await api.get('api/roles');
-    setRoles(data);
-  }
 }
 
 export default RolesTable;
