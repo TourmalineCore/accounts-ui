a4a53cb775d4e63cb41dfc74031a16d6eeeb28bb
Ekaterina <echekina@gmail.com>
2023-05-11 15:51:55 +0500

    test(roles-page): #85zrx4muu: add first test for RolesPageState

--- src/features/account-management/state/roles-page/RolesPageState.cy.tsx
+++ src/features/account-management/state/roles-page/RolesPageState.cy.tsx
@@ -1,5 +1,25 @@
 import '../../../../../cypress/support/commands';
+import RolesPageState from './RolesPageState';
 
 describe('RolesPageState', () => {
-  it('SHOULD WHEN', () => {});
+  it('SHOULD return all galleries WHEN initialized', () => {
+    const rolesPageState = new RolesPageState();
+
+    rolesPageState.initialize({
+      loadedRoles: [
+        {
+          id: 1,
+          name: 'Admin',
+          permissions: [],
+        },
+        {
+          id: 2,
+          name: 'Employee',
+          permissions: [],
+        },
+      ],
+    });
+
+    expect(rolesPageState.roles).to.has.lengthOf(2);
+  });
 });
