7b64d82829beecc3d2c6fb6259ec02d9570664bb
Ekaterina <echekina@gmail.com>
2023-05-12 18:13:20 +0500

    feat(roles-page): #85zrx4muu: add changeRoleName and cancelRoleEditing to state

--- src/features/account-management/state/roles-page/RolesPageState.tsx
+++ src/features/account-management/state/roles-page/RolesPageState.tsx
@@ -3,9 +3,13 @@ import { makeAutoObservable } from 'mobx';
 class RolesPageState {
   private _roles: Role[] = [];
 
+  private _roleBeforeEditing: Role | null = null;
+
   private _roleIdThatIsBeingEditedNow: number | null = null;
 
   private _isInEditMode: boolean = false;
+
+  private _newRoleName: string = '';
 
   constructor() {
     makeAutoObservable(this);
@@ -44,20 +48,48 @@ class RolesPageState {
   editRole(roleId: number) {
     this._roleIdThatIsBeingEditedNow = roleId;
     this._isInEditMode = true;
+    this._roleBeforeEditing = { ...this._roles[0] };
   }
 
-  applyChanges({ name, permissions } : { name: string; permissions: string[] }) {
-    this._roles = this._roles.map((role) => {
-      if (role.id === this._roleIdThatIsBeingEditedNow) {
-        return {
-          id: this._roleIdThatIsBeingEditedNow,
-          name,
-          permissions,
-        };
-      }
+  applyChanges({
+    name,
+    permissions,
+  } : {
+    name: string;
+    permissions: string[]
+  }) {
+    this._roles = this._roles
+      .map((role) => {
+        if (role.id === this._roleIdThatIsBeingEditedNow) {
+          return {
+            id: this._roleIdThatIsBeingEditedNow,
+            name,
+            permissions,
+          };
+        }
 
-      return role;
-    });
+        return role;
+      });
+  }
+
+  changeRoleName(newRoleName: string) {
+    const roleToChangeName = this._roles.find(({ id }) => id === this._roleIdThatIsBeingEditedNow);
+    roleToChangeName!.name = newRoleName;
+  }
+
+  cancelRoleEditing() {
+    this._roles = this._roles
+      .map((role) => {
+        if (role.id === this._roleIdThatIsBeingEditedNow) {
+          return this._roleBeforeEditing!;
+        }
+
+        return role;
+      });
+
+    this._roleIdThatIsBeingEditedNow = null;
+    this._isInEditMode = false;
+    this._roleBeforeEditing = null;
   }
 }
 
