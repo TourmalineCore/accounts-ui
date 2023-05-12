e6f964029afbe4ed8da84d32b05cd67d9bca1ce8
Ekaterina <echekina@gmail.com>
2023-05-12 12:04:44 +0500

    test(roles-page): #85zrx4muu: edit mode

--- src/features/account-management/state/roles-page/RolesPageState.cy.tsx
+++ src/features/account-management/state/roles-page/RolesPageState.cy.tsx
@@ -1,64 +1,49 @@
 import '../../../../../cypress/support/commands';
 import RolesPageState from './RolesPageState';
 
+const INITIAL_STATE = {
+  loadedRoles: [
+    {
+      id: 1,
+      name: 'Admin',
+      permissions: [],
+    },
+    {
+      id: 2,
+      name: 'Employee',
+      permissions: [],
+    },
+  ],
+};
+
 describe('RolesPageState', () => {
   it('SHOULD return all galleries WHEN initialized', () => {
     const rolesPageState = new RolesPageState();
 
-    rolesPageState.initialize({
-      loadedRoles: [
-        {
-          id: 1,
-          name: 'Admin',
-          permissions: [],
-        },
-        {
-          id: 2,
-          name: 'Employee',
-          permissions: [],
-        },
-      ],
-    });
+    rolesPageState.initialize(INITIAL_STATE);
 
     expect(rolesPageState.roles).to.has.lengthOf(2);
   });
 
-  it('SHOULD add new role to the beginning of the list WHEN adding was called', () => {
+  it('SHOULD add new role to the beginning of the list in edit mode WHEN adding was called', () => {
     const rolesPageState = new RolesPageState();
 
-    rolesPageState.initialize({
-      loadedRoles: [
-        {
-          id: 1,
-          name: 'Admin',
-          permissions: [],
-        },
-        {
-          id: 2,
-          name: 'Employee',
-          permissions: [],
-        },
-      ],
-    });
+    rolesPageState.initialize(INITIAL_STATE);
 
     rolesPageState.addNewRole();
 
-    expect(rolesPageState.roles).to.deep.equal([
-      {
-        id: 0,
-        name: '',
-        permissions: [],
-      },
-      {
-        id: 1,
-        name: 'Admin',
-        permissions: [],
-      },
-      {
-        id: 2,
-        name: 'Employee',
-        permissions: [],
-      },
-    ]);
+    expect(rolesPageState.roles).to.has.lengthOf(3);
+    expect(rolesPageState.roles[0].name).eq('');
+    expect(rolesPageState.isInEditMode).eq(true);
+    expect(rolesPageState.roleIdThatIsBeingEditedNow).eq(0);
+  });
+
+  it('SHOULD turn on edit mode for a role WHEN editing was called for it', () => {
+    const rolesPageState = new RolesPageState();
+
+    rolesPageState.initialize(INITIAL_STATE);
+
+    rolesPageState.editRole(2);
+    expect(rolesPageState.roleIdThatIsBeingEditedNow).eq(2);
   });
 });
