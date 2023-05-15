0438e5251c0722b2ddff213d79891088dc749dc9
Ekaterina <echekina@gmail.com>
2023-05-15 14:18:00 +0500

    test(roles-page): #85zrx4muu: unite creation and editing

--- cypress/e2e/pages/RolesPage.cy.ts
+++ cypress/e2e/pages/RolesPage.cy.ts
@@ -1,78 +1,34 @@
-describe('RolesPageState', () => {
-  it('SHOULD test happy path of role creation WHEN visit roles page', () => {
+describe('RolesPage', () => {
+  it('SHOULD test happy path of role creation, editind and deleting WHEN visiting roles page', () => {
     cy.visit('/account-management/roles-page');
 
     cy.getByData('roles-table')
       .should('exist');
 
+    // adding a role
     cy.getByData('add-new-role-button')
       .click();
 
-    cy.getByData('roles-name-input')
+    cy.getByData('role-name-input')
       .type('Manager1');
 
     cy.getByData('permission-checkbox')
       .first()
       .check();
 
-    cy.getByData('save-changes-button')
-      .click();
-
-    cy.getByData('roles-table')
-      .contains('Manager1');
-  });
-
-  it('SHOULD discard changes made to a role WHEN click cancel button', () => {
-    cy.visit('/account-management/roles-page');
-
-    cy.getByData('roles-table')
-      .should('exist');
-
-    cy.getByData('add-new-role-button')
-      .click();
-
-    cy.getByData('roles-name-input')
-      .type('Manager2');
-
-    cy.getByData('permission-checkbox')
-      .first()
-      .check();
-
-    cy.getByData('cancel-changes-button')
-      .click();
-
-    cy.getByData('roles-table')
-      .should('not.exist', 'Manager2');
-  });
-
-  it('SHOULD edit role WHEN editing is called for it', () => {
-    cy.visit('/account-management/roles-page');
-
-    cy.getByData('roles-table')
-      .should('exist');
-
-    cy.getByData('add-new-role-button')
-      .click();
-
-    cy.getByData('roles-name-input')
-      .type('Manager3');
-
-    cy.getByData('permission-checkbox')
-      .first()
-      .check();
-
+    // saving
     cy.getByData('save-changes-button')
       .click();
 
     // editing
-    cy.getByData('edit-role-button-Manager3')
+    cy.getByData('edit-role-button-Manager1')
       .click();
 
-    cy.getByData('roles-name-input')
-      .type('Manager4');
+    cy.getByData('role-name-input')
+      .type('Manager2');
 
     cy.getByData('roles-table')
-      .contains('Manager4');
+      .contains('Manager2');
   });
 });
 
