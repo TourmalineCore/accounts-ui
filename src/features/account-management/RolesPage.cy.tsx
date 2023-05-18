88dde1ce066af025ab9365329544d544e710d65b
Ekaterina <echekina@gmail.com>
2023-05-18 15:58:58 +0500

    test(roles-page): #85zrx4muu: add test to post changes on save

--- src/features/account-management/RolesPage.cy.tsx
+++ src/features/account-management/RolesPage.cy.tsx
@@ -35,7 +35,7 @@ describe('AddNewRole', () => {
   });
 
   it('SHOULD switch to edit mode WHEN adding a role is called', () => {
-    cy.intercept('GET', '*/roles', []).as('call-1');
+    cy.intercept('GET', '*/roles', []).as('call-2');
 
     cy.mount(
       <RolesPage />,
@@ -46,5 +46,36 @@ describe('AddNewRole', () => {
 
     cy.getByData('role-name-input')
       .should('exist');
+  });
+
+  it('SHOULD send changes made to a role to server WHEN saving them', () => {
+    cy.intercept('GET', '*/roles', []).as('call-3');
+
+    cy.mount(
+      <RolesPage />,
+    );
+
+    cy.getByData('add-new-role-button')
+      .click();
+
+    cy.getByData('role-name-input')
+      .type('Manager');
+
+    cy.getByData('save-changes-button')
+      .click();
+
+    cy.intercept(
+      'POST',
+      '*/roles/create',
+      {
+        name: 'Manager',
+        permissions: [],
+      },
+    ).as('call-4');
+
+    cy.intercept('GET', '*/roles', [{
+      name: 'Manager',
+      permissions: [],
+    }]).as('call-5');
   });
 });
