5177d2085981f05e462784db12ae648043050b9d
Ekaterina <echekina@gmail.com>
2023-05-18 15:59:35 +0500

    feat(roles-page): #85zrx4muu: add cancel and save buttons

--- src/features/account-management/RolesPageContent.tsx
+++ src/features/account-management/RolesPageContent.tsx
@@ -8,14 +8,37 @@ function RolesPageContent() {
 
   return (
     <div>
-      <button
-        type="button"
-        data-cy="add-new-role-button"
-        onClick={() => { rolesPageStateContext.addNewRole(); }}
-      >
-        Add new role
+      {
+        !rolesPageStateContext.isInEditMode
+          ? (
+            <button
+              type="button"
+              data-cy="add-new-role-button"
+              onClick={() => { rolesPageStateContext.addNewRole(); }}
+            >
+              Add new role
+            </button>
+          )
+          : (
+            <div>
+              <button
+                type="button"
+                data-cy="cancel-changes-button"
+                onClick={() => { }}
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                data-cy="save-changes-button"
+                onClick={() => { }}
+              >
+                Save Changes
+              </button>
+            </div>
+          )
+      }
 
-      </button>
       <RolesTable rolePermissions={rolesPageStateContext.roles} permissionGroups={[]} />
     </div>
   );
