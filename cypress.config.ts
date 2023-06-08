9b7877108377aedb6a473a53e747c6f009961573
Iliandr123 <isapronov@gmail.com>
2023-06-08 17:52:47 +0500

    chore: remove unnecessary cypress reporters

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'cypress';
 // @ts-ignore
-import mochawesomeWriter from 'cypress-mochawesome-reporter';
 
 export default defineConfig({
   e2e: {
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporterOptions.json',
-    },
     viewportWidth: 1920,
     viewportHeight: 1080,
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
@@ -15,9 +10,7 @@ export default defineConfig({
     defaultCommandTimeout: 30000,
     video: false,
     setupNodeEvents(on, config) {
-
     },
-    env: { MAILSLURP_API_KEY: 'd4297d96dcba82391396d35f1816ea1a84d1bf2b76bafec682756611cf383d00' },
   },
 
   component: {
