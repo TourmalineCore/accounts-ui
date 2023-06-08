a807e8fa056a61f68f963d7f503b83a2ff232275
sergiobagd <ssergioffx@gmail.com>
2023-06-08 16:33:31 +0500

    feat: #862jx66qg: add e2e mail sending test and clean up the junk

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,14 +8,16 @@ export default defineConfig({
     reporterOptions: {
       configFile: 'reporterOptions.json',
     },
-    viewportWidth: 1200,
-    viewportHeight: 660,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     baseUrl: 'https://tourmalinecore.github.io/React-Admin-Template',
+    defaultCommandTimeout: 30000,
     video: false,
     setupNodeEvents(on, config) {
-      mochawesomeWriter(on, config);
+
     },
+    env: { MAILSLURP_API_KEY: 'd4297d96dcba82391396d35f1816ea1a84d1bf2b76bafec682756611cf383d00' },
   },
 
   component: {
