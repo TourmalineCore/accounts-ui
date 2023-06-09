3e0f3b223853e8e57103cfc9b7b4afb3750ade88
Iliandr123 <isapronov@gmail.com>
2023-06-09 11:38:42 +0500

    refactor(e2e): refactor mailslurp tests

--- cypress/support/commands.tsx
+++ cypress/support/commands.tsx
@@ -1,19 +1,13 @@
 /// <reference types="cypress" />
 /// <reference types="cypress-mailslurp" />
 
-// import { MailSlurp } from 'mailslurp-client';
-
 Cypress.Commands.add('getByData', (selector) => cy.get(`[data-cy=${selector}]`));
 
 const apiKey = Cypress.env('MAILSLURP_API_KEY');
 if (!apiKey) {
   throw new Error(
-    'Error no MailSlurp API Key. Please set the `CYPRESS_MAILSLURP_API_KEY` '
+    'Error no MailSlurp API Key. Please set the `MAILSLURP_API_KEY` '
         + 'environment variable to the value of your MailSlurp API Key to use the MailSlurp Cypress plugin. '
         + 'Create a free account at https://app.mailslurp.com/sign-up/. See https://docs.cypress.io/guides/guides/environment-variables#Option-3-CYPRESS_ for more information.',
   );
 }
-
-// const mailslurp = new MailSlurp({ apiKey, basePath: 'https://cypress.api.mailslurp.com' });
-
-// Cypress.Commands.add('mailslurp', () => Promise.resolve(mailslurp));
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,12 +1,7 @@
 /// <reference types="cypress" />
 
-// import { MailSlurp } from 'mailslurp-client';
-
 declare namespace Cypress {
   interface Chainable {
-    // mailslurp: () => Promise<MailSlurp>;
     getByData(dataTestAttribute: string): Chainable<JQuery<HTMLElement>>;
-    // mailSlurp()x
-    // mailslurp(): Chainable<Promise<MailSlurp>>;
   }
 }
