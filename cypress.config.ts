a8a94cea4559091ac14f95347d583eb6882106e7
Iliandr <56068960+Iliandr123@users.noreply.github.com>
2023-07-20 09:52:28 +0500

    refactor(cypress): #85ztb4mmj: setting up cypress component tests for typescript (#25)
    * refactor(cypress): setting up cypress component tests for typescript
    
    * chore: fix error with lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,14 @@
-import { defineConfig } from 'cypress';
 // @ts-ignore
-import mochawesomeWriter from 'cypress-mochawesome-reporter';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporterOptions.json',
-    },
     viewportWidth: 1200,
     viewportHeight: 660,
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     baseUrl: 'https://tourmalinecore.github.io/React-Admin-Template',
     video: false,
     setupNodeEvents(on, config) {
-      mochawesomeWriter(on, config);
     },
   },
 
