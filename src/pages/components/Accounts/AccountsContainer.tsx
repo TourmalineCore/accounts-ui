100bce16b0926e16430fd0c02d2684b0daa0562d
Ekaterina <echekina@gmail.com>
2025-10-22 10:38:25 +0500

    refactor: move columns and actions to AccountsContent

--- src/pages/components/Accounts/AccountsContainer.tsx
+++ src/pages/components/Accounts/AccountsContainer.tsx
@@ -1,18 +1,15 @@
-import moment from 'moment'
-import clsx from 'clsx'
 import { useContext, useEffect, useState } from 'react'
-import { ColumnDef } from '@tanstack/table-core'
-import { ActionsType } from '@tourmalinecore/react-table-responsive'
-
 import { observer } from 'mobx-react-lite'
 import { toast } from 'react-toastify'
-import { api } from '../../../common/api'
-import { LINK_TO_ACCOUNT_SERVICE } from '../../../common/config/config'
 import { AccountManagementStateContext } from './state/AccountManagementStateContext'
+import { AccessBasedOnPemissionsStateContext } from '../../../routes/state/AccessBasedOnPemissionsStateContext'
+import { LINK_TO_ACCOUNT_SERVICE } from '../../../common/config/config'
+import { api } from '../../../common/api'
 import { AccountsContent } from './AccountsContent'
 
 export const AccountsContainer = observer(() => {
   const accountManagementState = useContext(AccountManagementStateContext)
+  const accessToChanges = useContext(AccessBasedOnPemissionsStateContext)
   const [
     isLoading,
     setIsLoading,
@@ -22,198 +19,13 @@ export const AccountsContainer = observer(() => {
     getAccountsAsync()
   }, [])
 
-  const columns: ColumnDef<Accounts>[] = [
-    {
-      header: `Name`,
-      id: `lastName`,
-      accessorFn: (row) => row.lastName,
-      enableSorting: true,
-      enableColumnFilter: true,
-      minSize: 300,
-      cell: ({
-        row, 
-      }) => {
-        const {
-          firstName, lastName, middleName, isBlocked,
-        } = row.original
-        return (
-          <div className={clsx(`account-management-page__account`, {
-            'account-management-page__account--isBlocked': isBlocked,
-          })}
-          >
-            {lastName}
-            {` `}
-            {firstName}
-            {` `}
-            {middleName || ``}
-          </div>
-        )
-      },
-    },
-    {
-      header: `Roles`,
-      id: `roles`,
-      accessorFn: (row) => row.roles,
-      cell: ({
-        row, 
-      }) => {
-        const {
-          roles, isBlocked, 
-        } = row.original
-        return (
-          <span className={clsx(`account-management-page__account`, {
-            'account-management-page__account--isBlocked': isBlocked,
-          })}
-          >
-            {roles.map((role, index) => (
-              <span key={role.id}>
-                {index > 0 ? `, ${role.name}` : role.name}
-              </span>
-            ))}
-          </span>
-        )
-      },
-    },
-    {
-      header: `Corporate Email`,
-      id: `corporateEmail`,
-      accessorFn: (row) => row.corporateEmail,
-      minSize: 300,
-      cell: ({
-        row, 
-      }) => {
-        const {
-          corporateEmail, isBlocked, 
-        } = row.original
-        return (
-          <span className={clsx(`account-management-page__account`, {
-            'account-management-page__account--isBlocked': isBlocked,
-          })}
-          >
-            {corporateEmail}
-          </span>
-        )
-      },
-    },
-    {
-      header: `Tenant`,
-      id: `tenantName`,
-      accessorFn: (row) => row.tenantName,
-      enableSorting: true,
-      minSize: 300,
-      cell: ({
-        row, 
-      }) => {
-        const {
-          tenantName, isBlocked, 
-        } = row.original
-        return (
-          <span
-            className={clsx(`account-management-page__account`, {
-              'account-management-page__account--isBlocked': isBlocked,
-            })}
-            data-cy="accounts-page-tenant-column"
-          >
-            {tenantName}
-          </span>
-        )
-      },
-    },
-    {
-      header: `Creation date (UTC)`,
-      id:`creationDate`,
-      accessorFn: (row) => row.creationDate,
-      enableSorting: true,
-      minSize: 250,
-      cell: ({
-        row, 
-      }) => {
-        const {
-          creationDate, isBlocked, 
-        } = row.original
-        const formattedDate = moment(creationDate)
-          .format(`DD.MM.YYYY HH:mm`)
-
-        return (
-          <span className={clsx(`account-management-page__account`, {
-            'account-management-page__account--isBlocked': isBlocked,
-          })}
-          >
-            {formattedDate}
-          </span>
-        )
-      },
-    },
-    {
-      header: `Status`,
-      id: `isBlocked`,
-      accessorFn: (row) => row.isBlocked, 
-      cell: ({
-        row, 
-      }) => {
-        const {
-          isBlocked, 
-        } = row.original
-
-        return (
-          <div
-            className="account-management-page__status"
-            data-cy="accounts-page-status-column"
-          >
-            {!isBlocked ? `Active` : `Blocked`}
-          </div>
-        )
-      },
-    },
-  ]
-
-  const actions: ActionsType<Accounts> = [
-    {
-      name: `edit`,
-      show: (row) => {
-        const {
-          isBlocked, canChangeAccountState, 
-        } = row.original
-
-        return !isBlocked && canChangeAccountState
-      },
-      renderText: () => `Edit`,
-      onClick: (_e, row) => window.location.href = `/account-management/accounts/edit/${row.original.id}`,
-    },
-    {
-      name: `block`,
-      show: (row) => {
-        const {
-          isBlocked, canChangeAccountState, 
-        } = row.original
-
-        return !isBlocked && canChangeAccountState
-      },
-      renderText: () => `Block`,
-      onClick: (_e, row) => blockAccountsAsync(row.original.id),
-    },
-    {
-      name: `unblock`,
-      show: (row) => {
-        const {
-          isBlocked, canChangeAccountState, 
-        } = row.original
-
-        return isBlocked && canChangeAccountState
-      },
-      renderText: () => `Unblock`,
-      onClick: (_e, row) => {
-        unblockAccountsAsync(row.original.id)
-        toast.dismiss(row.original.id)
-      },
-    },
-  ]
-
-  return (
+    return (
     <AccountsContent
-      columns={columns}
-      actions={actions}
       isLoading={isLoading}
+      accounts={accountManagementState.allAccounts}
+      accessPermissions={accessToChanges.accessPermissions}
+      onBlockAccount={blockAccountsAsync}
+      onUnblockAccount={unblockAccountsAsync}
     />
   )
 
