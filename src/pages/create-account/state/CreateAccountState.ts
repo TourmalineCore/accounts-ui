2b216da1df1b15da4edef7fe4e799022742a383a
Ekaterina <echekina@gmail.com>
2025-10-23 15:11:12 +0500

    feat: add private fields and getters in state

--- src/pages/create-account/state/CreateAccountState.ts
+++ src/pages/create-account/state/CreateAccountState.ts
@@ -1,15 +1,55 @@
 import { makeAutoObservable } from 'mobx'
 
+export const EMPTY_FORM_DATA: any = {
+  firstName: '',
+  lastName: '',
+  middleName: '',
+  corporateEmail: '',
+  tenantId: '',
+}
+
 export class CreateAccountState {
-  // private _allTenants: Tenants[] = []
+  private _triedToSubmit = false
+  private _selectedCheckboxes = new Set<string>([])
+  private _formData = {
+    firstName: '',
+    lastName: '',
+    middleName: '',
+    corporateEmail: '',
+    tenantId: '',
+  }
+  private _rolesData: { [key: number]: string } = {}
+  private _tenantsData: Tenants[] = []
+  private _hasError = false
 
   constructor() {
     makeAutoObservable(this)
   }
 
-  // get allTenants() {
-  //   return this._allTenants
-  //     .slice()
+  get triedToSubmit() {
+    return this._triedToSubmit
+  }
+
+  get selectedCheckboxes() {
+    return this._selectedCheckboxes
+  }
+
+  get formData() {
+    return this._formData
+  }
+
+  get rolesData() {
+    return this._rolesData
+  }
+
+  get tenantsData() {
+    return this._tenantsData
+  }
+
+  get hasError() {
+    return this._hasError
+  }
+
   // }
 
   // getTenants(newTenant: Tenants[]) {
