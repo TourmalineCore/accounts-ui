abb63192741563d337baa8b82cada1822737ccc2
Ekaterina <echekina@gmail.com>
2025-10-23 15:08:03 +0500

    test: add init & formData tests for state

--- src/pages/create-account/state/CreateAccountState.cy.ts
+++ src/pages/create-account/state/CreateAccountState.cy.ts
@@ -1,5 +1,74 @@
 import '../../../../cypress/support/commands'
 
+import { CreateAccountState, EMPTY_FORM_DATA } from "./CreateAccountState"
+
+describe(`CreateAccountState`, () => {
+  describe(`Initialization`, initializationTests)
+  describe(`Form Data`, formDataTests)
+})
+
+function initializationTests() {
+  const createAccountState = new CreateAccountState()
+  
+  it(`
+  GIVEN a CreateAccountState
+  WHEN initialize
+  SHOULD have default values
+  `, () => {
+    expect(createAccountState.triedToSubmit)
+      .to
+      .be
+      .false
+    expect(createAccountState.selectedCheckboxes.size)
+      .to
+      .eq(0)
+    expect(createAccountState.formData)
+      .to
+      .deep
+      .eq(EMPTY_FORM_DATA)
+    expect(createAccountState.rolesData)
+      .to
+      .deep
+      .eq({})
+    expect(createAccountState.tenantsData)
+      .to
+      .deep
+      .eq([])
+    expect(createAccountState.hasError)
+      .to
+      .be
+      .false
+    
+  })
+}
+
+function formDataTests() {
+  let createAccountState: CreateAccountState
+
+   const accountDataForInitialization = {
+    firstName: 'Ceo',
+    lastName: 'Ceo',
+    middleName: 'Ceo',
+    corporateEmail: 'ceo@tourmalinecore.com',
+    tenantId: '0',
+  }
+
+  beforeEach(() => {
+    createAccountState = new CreateAccountState()
+  })
+
+  it(`
+  GIVEN the CreateAccountState
+  WHEN set account form data
+  SHOULD display new values in the account object
+  `, () => {
+    createAccountState.setFormData(accountDataForInitialization)
+
+    expect(createAccountState.formData)
+    .to
+    .deep
+    .eq(accountDataForInitialization)
+  })
 // import { CreateAccountState } from './CreateAccountState'
 
 // const INITIAL_STATE: Tenants[] = [
