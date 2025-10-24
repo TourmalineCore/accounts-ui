b680861e329dfed7fdb5de1552d5cfafbe5b5f6d
Ekaterina <echekina@gmail.com>
2025-10-24 19:24:08 +0500

    chore: cleanup

--- src/pages/create-account/state/CreateAccountState.cy.ts
+++ src/pages/create-account/state/CreateAccountState.cy.ts
@@ -272,25 +272,3 @@ function isErrorTests() {
       .false
   })
 }
-// import { CreateAccountState } from './CreateAccountState'
-
-// const INITIAL_STATE: Tenants[] = [
-//   {
-//     id: 1,
-//     name: `Blue`,
-//   },
-//   {
-//     id: 2,
-//     name: `Black`,
-//   },
-// ]
-
-describe(`CreateAccountState`, () => {
-  // it(`SHOULD return all tenants WHEN initialized`, () => {
-  //   const tenantManagementState = new CreateAccountState()
-
-  //   tenantManagementState.getTenants(INITIAL_STATE)
-
-  //   expect(tenantManagementState.allTenants).to.has.lengthOf(2)
-  // })
-})
