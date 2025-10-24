68781e0d12c3690763fec6043c2169bd4e9d3aa5
Ekaterina <echekina@gmail.com>
2025-10-24 18:43:46 +0500

    feat: extract CreateTenantContent from Container

--- src/pages/create-tenant/CreateTenantContainer.tsx
+++ src/pages/create-tenant/CreateTenantContainer.tsx
@@ -1,78 +1,30 @@
-import { Input } from '@tourmalinecore/react-tc-ui-kit'
-import { ChangeEvent, useState } from 'react'
-import { toast } from 'react-toastify'
+import { observer } from 'mobx-react-lite'
+import { useContext } from 'react'
+import { CreateTenantStateContext } from './state/CreateTenantStateContext'
+import { CreateTenantContent } from './CreateTenantContent'
 import { api } from '../../common/api'
 import { LINK_TO_ACCOUNT_SERVICE } from '../../common/config/config'
+import { toast } from 'react-toastify'
 
-export function CreateTenantContainer() {
-
-  const [
-    triedToSubmit,
-    setTriedToSubmit,
-  ] = useState(false)
-  const [
-    formData,
-    setFormData,
-  ] = useState({
-    name: ``,
-  })
+export const CreateTenantContainer = observer(() => {
+  const createTenantState = useContext(CreateTenantStateContext)
 
   return (
-    <div className="create-tenant"
-      data-cy="create-tenant-page">
-      <h1 className="heading create-tenant__title">Add New Tenant</h1>
-
-      <div className="create-tenant__inner">
-        <div className="create-tenant__box">
-          <span>Name</span>
-          <Input
-            data-cy="create-tenant-page-input"
-            value={formData.name}
-            isInvalid={!formData.name && triedToSubmit}
-            validationMessages={[
-              `This field is required. Please fill it up.`,
-            ]}
-            isMessagesAbsolute
-            maxLength={50}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setFormData({
-              ...formData,
-              name: e.target.value.trim(), 
-            })}
-          />
-        </div>
-        <div className="create-tenant__inner-button">
-          <button
-            type="button"
-            data-cy="create-tenant-page-button-cancel"
-            className="create-account__button"
-            onClick={() => window.location.href =`/account-management/tenants`}
-          >
-            Cancel
-          </button>
-
-          <button
-            type="button"
-            data-cy="create-tenant-page-button-add"
-            className="create-account__button"
-            onClick={() => createTenantAsync()}
-          >
-            Add
-          </button>
-        </div>
-      </div>
-    </div>
+    <CreateTenantContent 
+      createTenantAsync={createTenantAsync} 
+    />
   )
 
   async function createTenantAsync() {
-    setTriedToSubmit(true)
+    createTenantState.setIsTriedToSubmit(true)
 
-    if (formData.name) {
+    if (createTenantState.formData.name) {
       try {
         await api.post<TenantCreate>(`${LINK_TO_ACCOUNT_SERVICE}tenants`, {
-          ...formData,
+          ...createTenantState.formData,
         })
 
-        setTriedToSubmit(false)
+        createTenantState.setIsTriedToSubmit(false)
         window.location.href = `/account-management/tenants`
 
         toast(`New tenant added successfully`, {
@@ -88,4 +40,4 @@ export function CreateTenantContainer() {
       }
     }
   }
-}
+})
